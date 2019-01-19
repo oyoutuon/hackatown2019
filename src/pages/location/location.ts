cd913b790141ac5e1b711c159ed478e22b333556
hnspn <bobmatt911@gmail.com>
2019-01-19 17:10:32 -0500

    showing location data

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
-import { Location } from "../../../common/location";
+import { Location, mockLocation } from "../../../common/location";
 
 @Component({
   selector: "page-location",
@@ -8,12 +8,10 @@ import { Location } from "../../../common/location";
 })
 export class LocationPage {
   lat: number = 45.5017;
-  lng: number = 73.5673;
-  location: Location;
+  lng: number = -73.5673;
+  location: Location = mockLocation;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad LocationPage");
-  }
+  ionViewDidLoad() {}
 }
