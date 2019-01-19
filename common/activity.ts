ec8f8adac7d6fe40c5ac1a36970516d8342a19da
hnspn <bobmatt911@gmail.com>
2019-01-19 16:24:59 -0500

    errors TODO: fix references

--- common/activity.ts
+++ common/activity.ts
@@ -4,8 +4,6 @@ export interface Activity {
   sport: string;
   location: Location;
   price: number;
-  startTime: Date;
-  endTime: Date;
   type: string;
   traffic: string;
   manager: string;
@@ -17,8 +15,7 @@ export const mockPunctualActivty: PunctualActivity = {
   sport: "Basukettuboru",
   location: mockLocation,
   price: 5,
-  startTime: new Date(),
-  endTime: new Date(),
+  time: { dotw: "Sunday", startTime: new Date(), endTime: new Date() },
   manager: "Michel Gagnon",
   traffic: "moderate",
   type: "free play",
@@ -31,16 +28,31 @@ export const mockSubscriptionActivty: SubscriptionActivity = {
   sport: "Socceru",
   location: mockLocation,
   price: 115,
-  startTime: new Date(),
-  endTime: new Date(),
+  period: { startTime: new Date(), endTime: new Date() },
   manager: "Zinedine Zidane",
   traffic: "none",
   type: "free play",
   description: "fasdf asdf asd f asdfas dcanoiszhalsdn sopd g$%^#$W",
+  times: [
+    { dotw: "Sunday", startTime: new Date(), endTime: new Date() },
+    { dotw: "Moday", startTime: new Date(), endTime: new Date() },
+    { dotw: "Friday", startTime: new Date(), endTime: new Date() }
+  ],
   imgUrl:
     "https://images.unsplash.com/photo-1521731978332-9e9e714bdd20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
 };
 
-export interface SubscriptionActivity extends Activity {}
+export interface SubscriptionActivity extends Activity {
+  period: DateRange;
+  times: DateRange[];
+}
 
-export interface PunctualActivity extends Activity {}
+export interface PunctualActivity extends Activity {
+  time: DateRange;
+}
+
+export interface DateRange {
+  dotw?: string;
+  startTime: Date;
+  endTime: Date;
+}
--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ResultsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import {
+  mockPunctualActivty,
+  mockSubscriptionActivty,
+  Activity
+} from "../../../common/activity";
+import { SubscriptionActivityPage } from "../subscription-activity/subscription-activity";
 
 @Component({
-  selector: 'page-results',
-  templateUrl: 'results.html',
+  selector: "page-results",
+  templateUrl: "results.html"
 })
 export class ResultsPage {
+  results: Activity[] = [mockPunctualActivty, mockSubscriptionActivty];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {}
+
+  onSelect(result: Activity) {
+    const page = SubscriptionActivityPage;
+    this.navCtrl.push(page);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResultsPage');
-  }
-
 }
