0a7fa574b00a1bd52a2c2a22a31d21dbc415f76f
StevenLam <lam.steven7@gmail.com>
2019-01-19 17:39:31 -0500

    sl: first iteration of the search form

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -13,6 +13,25 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search.html',
 })
 export class SearchPage {
+  mockTypes = [
+    'Freeplay',
+    'Beginner Course',
+    'Intermediate Course',
+    'Advanced Course'
+  ]
+
+  mockTraffics = [
+    'Usually Empty',
+    'Some Waiting Time',
+    'Very Busy'
+  ]
+
+  sport: string = 'BASUKETU';
+  location: string = 'Chez marcus';
+  price: number = 5;
+  type: string = this.mockTypes[0];
+  traffic: string = this.mockTraffics[0];
+  manager: string = "Michel Gagnon";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
