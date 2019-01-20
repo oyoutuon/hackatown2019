44cb1d2880e1020b83c73f0107f36baa680ed46f
Steven Lam <lam.steven7@gmail.com>
2019-01-20 05:50:40 -0500

    sl: implemented activity page

--- manager/src/pages/activity/activity.ts
+++ manager/src/pages/activity/activity.ts
@@ -1,28 +1,6 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
-import { Activity } from "../../../../common/activity";
-
-const mockLocation = {
-  activities: [],
-  name: "Chez marcus",
-  address: "environ",
-  phoneNumber: "+1 514 322 4221",
-  email: "marcus.phan@polymtl.ca"
-};
-
-const mockActivity = [
-  {
-    sport: "Competitive eating",
-    location: mockLocation,
-    price: 0,
-    type: "free",
-    traffic: "some",
-    manager: "Hanifa Boucheneb",
-    description: "marcus ate all of the food.",
-    imgUrl: "",
-    time: { startTime: new Date(), endTime: new Date() }
-  }
-];
+import { Activity, SubscriptionActivity } from "../../../../common/activity";
 
 @Component({
   selector: "page-activity",
@@ -32,8 +10,12 @@ export class ActivityPage {
   activity: Activity;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.activity = this.navParams.get("activity") || mockActivity[0];
+    this.activity = this.navParams.get("activity");
   }
 
   ionViewDidLoad() {}
+
+  isSubscription(result: any): result is SubscriptionActivity {
+    return result.period !== undefined;
+  }
 }
