9fab9bd5af9a5b44da13e80df2ce20a8e5042f46
Olivia You-Tuon <olivia.you-tuon@polymtl.ca>
2019-01-19 23:17:43 -0500

    filter search results

--- client/src/pages/results/results.ts
+++ client/src/pages/results/results.ts
@@ -10,11 +10,12 @@ import { Location } from "../../../../common/location";
   templateUrl: "results.html"
 })
 export class ResultsPage {
-  results: Activity[] = mockActivities;
+  results: Activity[];
 
   parks: Location[] = mockParks;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(this.results);
+    this.results = this.navParams.get('results');
   }
 
   ionViewDidLoad() {}
