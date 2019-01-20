42014c9bdd1036b17f32fe329ab3480fe128d2d0
hnspn <bobmatt911@gmail.com>
2019-01-20 11:50:27 -0500

    default mangaer page is landing

--- manager/src/app/app.component.ts
+++ manager/src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { DebugPage } from '../pages/debug/debug';
+// import { DebugPage } from "../pages/debug/debug";
+import { LandingPage } from "../pages/landing/landing";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = DebugPage;
+  rootPage: any = LandingPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +24,3 @@ export class MyApp {
     });
   }
 }
-
