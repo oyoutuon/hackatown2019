95513b2cce778ac97a2956821bd945362d78f472
hnspn <bobmatt911@gmail.com>
2019-01-19 22:15:22 -0500

    added agm to city dashboard

--- city-dashboard/src/app/app.module.ts
+++ city-dashboard/src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SuiModule } from "ng2-semantic-ui";
 import { ChartsModule } from "ng2-charts";
+import { AgmCoreModule } from "@agm/core";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SuiModule, ChartsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SuiModule,
+    ChartsModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyAuFeSreL0UefHrw_HKXkXeYZPtapIJdMg"
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
